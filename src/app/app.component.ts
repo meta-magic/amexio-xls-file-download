7dc0f33cf48d673dc7942d30345ab7deb9d9aee3
ketangote <ketan.gote@metamagic.in>
2020-08-10 13:33:27 +0530

    Amexio File Upload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { ExcelService } from './service/excel.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'amexio-xls-file-export';
+export class AppComponent implements OnInit{
+  title = 'amexio xls file export';
+
+  data: any [] = [];
+  constructor(private http: HttpClient, private excelService: ExcelService) {
+
+  }
+
+  ngOnInit(){
+    this.loadData();
+  }
+
+  loadData(){
+    this.http.get("assets/data/country.json").subscribe((response:any)=>{
+      this.data = response.data;
+    });
+  }
+
+  exportXlS(event: any) {
+    debugger;
+    this.excelService.exportAsExcelFile(this.data, 'sample');
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AmexioWidgetModule } from "amexio-ng-extensions"; // Import Amexio library
+import { FormsModule } from '@angular/forms';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { ExcelService } from './service/excel.service';
 
 @NgModule({
   declarations: [
@@ -10,9 +14,12 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AmexioWidgetModule,
+    FormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [ExcelService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
